e13d4bfe413b3d6308cb7c0301cee52dabe575b4
Fernando Ramos <fernando-ramos94@hotmail.com>
2025-05-23 17:16:59 -0300

    Add handleCopy in colorBox

--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -3,11 +3,20 @@ import styles from './ColorBox.module.css'
 
 
 export default class ColorBox extends Component{
+  constructor(props){
+    super(props)
+
+    this.handleCopy=this.handleCopy.bind(this)
+  }
+
+  handleCopy(){
+    {navigator.clipboard.writeText(this.props.background)}
+  }
 
   render(){
     const {name,background} = this.props
     return(
-      <div style={{background}} className={styles.colorBox}>
+      <div onClick={this.handleCopy} style={{background}} className={styles.colorBox}>
         <div className={styles.copy_container}>
           <div className={styles.box_content}>
             <span>{this.props.name}</span>
