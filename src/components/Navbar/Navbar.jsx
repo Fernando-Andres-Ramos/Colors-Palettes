4383a0e96ffaad9a698826351fc3e214d6d0a783
Fernando Ramos <fernando-ramos94@hotmail.com>
2025-05-24 21:34:42 -0300

    add select and material UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import styles from './Navbar.module.css'
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 
 export default class Navbar extends Component{
   constructor(props){
     super(props)
 
+    this.state = {format:"hex"}
+
     this.handleChange = this.handleChange.bind(this)
+    this.handleSelectChange = this.handleSelectChange.bind(this)
   }
 
   handleChange(e){
     this.props.changeLevel(e.target.value)
   }
+
+  handleSelectChange(e){
+    this.setState({format:e.target.value})
+    this.props.changeSelectValue(e.target.value)
+  }
+  
 
   render(){
     const {defaultValue} = this.props
@@ -31,6 +42,13 @@ export default class Navbar extends Component{
               step="100"
               className={styles.slider}/>
           </div>
+        </div>
+        <div className={styles.select_container}>
+          <Select value={this.state.format} onChange={this.handleSelectChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,0)</MenuItem>
+          </Select>
         </div>
       </header>
     )
--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -8,24 +8,33 @@ export default class Palette extends Component{
 
   constructor(props){
     super(props);
-    this.state={level:500}
+    this.state={level:500,format:"hex"}
 
     this.changeLevel = this.changeLevel.bind(this)
+    this.changeSelectValue=this.changeSelectValue.bind(this)
+    
   }
 
   changeLevel(level){
     this.setState({level})
   }
 
+  changeSelectValue(value){
+    this.setState({format:value})
+  }
+
   render(){
     const {colors} = this.props.palette
-    const {level} = this.state
+    const {level,format} = this.state
     const colorBoxes = colors[this.state.level].map(color=>(
-      <ColorBox background={color.hex} name={color.name}/>
+      <ColorBox background={color[format]} name={color.name}/>
     ))
     return(
       <div className={styles.palette}>
-        <Navbar defaultValue={level} changeLevel={this.changeLevel}/>
+        <Navbar 
+          defaultValue={level} 
+          changeLevel={this.changeLevel} 
+          changeSelectValue={this.changeSelectValue}/>
         <div className={styles.palette_colors}>{colorBoxes}</div>
         {/* footer eventually */}
       </div>
